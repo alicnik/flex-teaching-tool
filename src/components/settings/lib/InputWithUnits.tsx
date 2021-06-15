60c4b58d035ca26f6295e652256665b7a243492e
Alex Nicholas <me@alexnicholas.dev>
2021-06-15 20:33:21 +0100

    remove units

--- src/components/settings/lib/InputWithUnits.tsx
+++ src/components/settings/lib/InputWithUnits.tsx
@@ -3,13 +3,12 @@ import {
   FormHelperText,
   InputAdornment,
   makeStyles,
-  MenuItem,
   OutlinedInput,
-  Select,
 } from '@material-ui/core';
 import * as React from 'react';
 
 type PropertyName = 'gap';
+
 interface InputWithUnitsProps {
   property: PropertyName;
   value: string;
@@ -19,15 +18,13 @@ interface InputWithUnitsProps {
 
 export function InputWithUnits({ property, value, onChange, helperText }: InputWithUnitsProps) {
   const styles = useStyles();
-  const [unit, setUnit] = React.useState('px');
-  const handleUnitChange = (e: React.ChangeEvent<{ value: unknown }>) =>
-    setUnit(e.target.value as string);
+
   return (
     <FormControl variant="outlined" margin="dense" className={styles.formControl}>
       <OutlinedInput
         value={value}
         onChange={onChange(property)}
-        endAdornment={<UnitPicker unit={unit} onChange={handleUnitChange} />}
+        endAdornment={<InputAdornment position="end">px</InputAdornment>}
         labelWidth={0}
       />
       {helperText && <FormHelperText>{helperText}</FormHelperText>}
@@ -40,32 +37,4 @@ const useStyles = makeStyles({
     display: 'block',
     marginBottom: 20,
   },
-  'MuiInput-underline': {
-    '&::before, &::after': {
-      all: 'unset',
-    },
-  },
 });
-
-interface UnitPickerProps {
-  unit: string;
-  onChange: (e: React.ChangeEvent<{ value: unknown }>) => void;
-}
-
-function UnitPicker({ unit, onChange }: UnitPickerProps) {
-  const styles = useStyles();
-
-  return (
-    <InputAdornment position="end">
-      <Select value={unit} onChange={onChange} className={styles['MuiInput-underline']}>
-        {validCssUnits.map((cssUnit) => (
-          <MenuItem key={cssUnit} value={cssUnit}>
-            {cssUnit}
-          </MenuItem>
-        ))}
-      </Select>
-    </InputAdornment>
-  );
-}
-
-const validCssUnits = ['px', 'rem', 'em'];
